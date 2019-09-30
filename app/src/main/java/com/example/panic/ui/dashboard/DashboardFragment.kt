9740e9ec45633e64bd830ba7520069e8996d357c
jonnatas <jonatas_lenon@hotmail.com>
2019-09-30 13:59:41 -0300

    first commit

---
+++ app/src/main/java/com/example/panic/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,26 @@
+package com.example.panic.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.panic.R
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        var text_dashboard = root.findViewById<TextView>(R.id.text_dashboard);
+        text_dashboard.setText("Dashboard Fragment")
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/panic/ui/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.panic.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.panic.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        var text_home = root.findViewById<TextView>(R.id.text_home)
+
+        text_home.setText("Home Fragment")
+
+        return root
+    }
+}
